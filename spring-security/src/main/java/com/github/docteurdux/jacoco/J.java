d17c1b6bac106155006d0ff9b4f4333af810b59f
docteurdux <xavierdpt+docteurdux@gmail.com>
2017-09-22 13:39:59 +0200

    jacoco

---
+++ spring-security/src/main/java/com/github/docteurdux/jacoco/J.java
@@ -0,0 +1,10 @@
+package com.github.docteurdux.jacoco;
+
+public class J {
+	public static int foo(int x) {
+		if (x == 7) {
+			return 21;
+		}
+		return 2 * x;
+	}
+}
---
+++ spring-security/src/test/java/com/github/docteurdux/jacoco/JTest.java
@@ -0,0 +1,13 @@
+package com.github.docteurdux.jacoco;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JTest {
+
+	@Test
+	public void testFoo() {
+		Assert.assertEquals(J.foo(3), 6);
+	}
+
+}
