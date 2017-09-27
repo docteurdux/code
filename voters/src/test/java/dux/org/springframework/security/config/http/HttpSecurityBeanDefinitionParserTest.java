25512e4fee6b78847aca176f68d37a72bbd57b24
docteurdux <xavierdpt+docteurdux@gmail.com>
2017-09-27 22:04:52 +0200

    .

---
+++ voters/src/test/java/dux/org/springframework/security/config/http/HttpSecurityBeanDefinitionParserTest.java
@@ -0,0 +1,11 @@
+package dux.org.springframework.security.config.http;
+
+import org.junit.Test;
+import org.springframework.security.config.http.HttpSecurityBeanDefinitionParser;
+
+public class HttpSecurityBeanDefinitionParserTest {
+	@Test
+	public void test() {
+		HttpSecurityBeanDefinitionParser parser = new HttpSecurityBeanDefinitionParser();
+	}
+}
