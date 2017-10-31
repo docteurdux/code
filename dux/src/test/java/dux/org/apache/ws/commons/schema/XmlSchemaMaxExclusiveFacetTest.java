a383ae15e83c16d004bf634fb50bbf43a696443b
docteurdux <xavierdpt+docteurdux@gmail.com>
2017-10-31 08:58:34 +0100

    .

--- dux/src/test/java/dux/org/apache/ws/commons/schema/XmlSchemaMaxExclusiveFacetTest.java
+++ dux/src/test/java/dux/org/apache/ws/commons/schema/XmlSchemaMaxExclusiveFacetTest.java
@@ -5,7 +5,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.github.docteurdux.test.AbstractTest;
+import com.github.docteurdux.test.Focus;
 
+@Focus
 public class XmlSchemaMaxExclusiveFacetTest extends AbstractTest {
 
 	@Before
